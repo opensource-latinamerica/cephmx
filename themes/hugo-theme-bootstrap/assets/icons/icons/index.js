3c91ca85296f7c5f9541f13b8ca86d0ccdaf19ed
alsotoes <alsotoes@gmail.com>
2026-04-20 14:36:39 -0600

    chore: fix theme submodule

--- themes/hugo-theme-bootstrap/assets/icons/icons/index.js
+++
@@ -1,8 +0,0 @@
-import faBilibili from "./faBilibili";
-import faCodeberg from "./faCodeberg";
-import faLiberapay from "./faLiberapay";
-import faOffline from "./faOffline";
-import faTipeee from "./faTipeee";
-import faXTwitter from "./faXTwitter";
-
-export { faBilibili, faCodeberg, faLiberapay, faOffline, faTipeee, faXTwitter };
